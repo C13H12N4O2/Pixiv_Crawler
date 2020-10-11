58395d7b19c99e79c5a6b791c8ff77d00539280a
C13H12N4O2 <c13h12n4o2@C13H12N4O2s-MacBook-Pro.local>
2020-10-11 20:05:27 +0900

    Upload Pixiv_Crawler

---
+++ Pixiv_Crawler.py
@@ -0,0 +1,114 @@
+import requests
+import os
+from bs4 import BeautifulSoup as bs
+
+class Pixiv():
+    def __init__(self):
+        self.s = requests.Session()
+        self.base_url = 'https://accounts.pixiv.net/login'
+        self.main_url = 'https://www.pixiv.net'
+        self.setting_url = 'https://www.pixiv.net/setting_profile.php'
+        self.artworks_url = 'https://www.pixiv.net/artworks/'
+        self.ranking_url = 'https://www.pixiv.net/ranking.php?mode=daily&content=illust'
+        
+        self.headers = {
+            'referer': 'https://accounts.pixiv.net',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+            'connection': 'keep-alive'
+        }
+        self.login_info = {}
+        self.pixiv_id = 'pixiv_id'
+        self.password = 'pixiv_pw'
+        self.post_key = []
+        self.return_to = 'http://www.pixiv.net'
+        self.cookies = []
+        self.current_path = './Pixiv/'
+        
+    def login(self):
+        post_key_html = self.s.get(self.base_url, headers = self.headers).text
+        post_key_soup = bs(post_key_html, 'html.parser')
+        self.post_key = post_key_soup.find('input', {'name': 'post_key'})
+        self.login_info = {
+            'pixiv_id': self.pixiv_id,
+            'password': self.password,
+            'post_key': self.post_key['value'],
+            'return_to': self.return_to
+        }
+        login_req = self.s.post(self.base_url, data = self.login_info, headers = self.headers)
+        self.cookies =  self.s.cookies.get_dict()
+        self.headers = {**self.headers, **self.cookies}
+        print(login_req.status_code)
+    
+    def get_artwork_url(self):
+        ranking_html = self.s.get(self.ranking_url, data = self.login_info, headers = self.headers).text
+        ranking_data = bs(ranking_html, 'html.parser').find('div', {'class': 'ranking-items-container'}).find_all('section')
+        today_data = bs(ranking_html, 'html.parser').find_all('a', {'class': 'current'})
+        
+        for html in today_data:
+            if html['href'].find('date', 4) != -1:
+                today = html['href'].split('=')[-1]
+        self.current_path += today + '/'
+        self.mkdir()
+        
+        for itr in ranking_data:
+            print('{rank}: {title} by {name}[{id}]'.format(rank = itr['data-rank'], title  = itr['data-title'], name = itr['data-user-name'], id = itr['data-id']))
+            self.get_img_url(itr['data-rank'], itr['data-title'], itr['data-user-name'], itr['data-id'])
+            print()
+            
+    def get_img_url(self, data_rank, data_title, user_name, data_id):
+        target_html = self.s.get(self.artworks_url + data_id, headers = self.headers).text
+        target_soup = bs(target_html, 'html.parser').find_all('meta')
+        target_content = target_soup[-1]['content']
+        target_content = target_content.split(',')
+                
+        target_url = ''
+        for html in target_content:
+            if html.find('"original"') != -1:
+                target_url = max(target_url, html, key = len)
+        target_url = max(target_url.split('"'), key = len)
+        
+        index = 0
+        img_url = target_url
+        while (True):
+            img_url = target_url.split('.')
+            img_format = img_url[-1]
+            if index != 0:
+                img_page = img_url[-2].split('_')
+                img_page[-1] = img_page[-1].replace('0', '{index}'.format(index = index))
+                img_url[-2] = '_'.join(img_page)
+            img_url = '.'.join(img_url)
+                
+            img_data = self.s.get(img_url, headers = self.headers)
+            if img_data.status_code != 200:
+                break;
+            
+            if index == 0:
+                file_name = '#{rank} - {title} by {name}.{format}'.format(rank = data_rank, title = data_title, name = user_name, format = img_format)
+            else:
+                file_name = '#{rank} - {title}[{index}] by {name}.{format}'.format(rank = data_rank, title = data_title, index = index, name = user_name, format = img_format)
+            self.download_img(img_data, file_name)
+            print(file_name)
+                
+            index += 1
+        
+    def download_img(self, img_data, file_name):
+        try:
+            with open(os.path.join(self.current_path, file_name), 'wb') as handle:
+                handle.writelines(img_data)
+                handle.close()
+        except FileNotFoundError:
+            print('File name [{file_name}] is too long!'.format(file_name = file_name))
+            print('Download failed')
+        
+    def mkdir(self):
+        has_path = os.path.exists(self.current_path)
+        
+        if has_path:
+            print('The folder already exists')
+        else:
+           os.makedirs(self.current_path)
+        
+if __name__ == '__main__':
+    pixiv = Pixiv()
+    pixiv.login()
+    pixiv.get_artwork_url()
